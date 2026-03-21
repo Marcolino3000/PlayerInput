2eee6bd714226df24b20adeabac5ffbf93ec9798
MarvinWack <marc.uhlherr@gmail.com>
2026-03-21 16:32:21 +0100

    playercontroller handles complete movement

--- Runtime/PlayerController.cs
+++ Runtime/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,15 +8,18 @@ namespace Runtime.Scripts.PlayerInput
     public class PlayerController : MonoBehaviour
     {
         public event Action OnInteractionTriggered;
-        public event Action<bool, MoveDirection> OnMovementStarted;
+        public event Action<MoveDirection> OnMovementStarted;
         public event Action OnMovementEnded; 
         
         [SerializeField] private float speed = 30f;
+        [SerializeField] private float distanceThreshold;
         [SerializeField] private Rigidbody rb;
 
         private bool isMoving;
-        private Coroutine moveByClickCoroutine;
+        private Coroutine moveCoroutine;
+        private MoveDirection lastMoveDirection;
         private static bool movementEnabled = true;
+        private Vector2 Position => new(transform.position.x, transform.position.z);
 
         public static void EnableMovement(bool value)
         {
@@ -47,35 +51,56 @@ namespace Runtime.Scripts.PlayerInput
             OnMovementEnded?.Invoke();
         }
 
-        public void OnMove(Vector2 moveDirection)
+        public void MoveInDirection(Vector2 direction)
         {
-            // StopCoroutine(moveByClickCoroutine);
-            // moveByClickCoroutine = moveCoroutine;
-            // if (moveByClickCoroutine != null)+
+            OnMove(direction);
+        }
+        
+        public IEnumerator MoveToTargetPosition(Vector2 targetPosition)
+        {
+            yield return MoveByPosition(targetPosition);
+        }
+
+        private IEnumerator MoveByPosition(Vector2 targetPosition)
+        {
+            while (Vector2.Distance(targetPosition, Position) > distanceThreshold)
+            {
+                Vector3 direction = (targetPosition - Position);
+                Vector2 moveInput = new Vector2(direction.x, direction.z).normalized;
+                OnMove(moveInput);
+                yield return null;
+            }
             
-            
+            StopMoving();
+        }
+        
+
+        private void OnMove(Vector2 moveDirection)
+        {
             if(!movementEnabled)
             {
                 StopMoving();
                 return;
             }
             
+            var direction = moveDirection.x < 0 ? MoveDirection.Left : MoveDirection.Right;
+
             bool wasMoving = isMoving;
             isMoving = moveDirection.sqrMagnitude > 0.01f;
 
-            if (isMoving != wasMoving)
+            if (!isMoving)
             {
-                if (isMoving)
-                {
-                    OnMovementStarted?.Invoke(isMoving, moveDirection.x < 0 ? MoveDirection.Left : MoveDirection.Right);
-                }
-                else
-                {
-                    OnMovementEnded?.Invoke();
-                }
+                StopMoving();
+                return;
+            }
+            
+            if (!wasMoving || direction != lastMoveDirection)
+            {
+                OnMovementStarted?.Invoke(direction);
             }
             
             rb.linearVelocity = new Vector3(moveDirection.x * speed, 0, moveDirection.y * speed);
+            lastMoveDirection = direction;
         }
     }
 
@@ -83,6 +108,12 @@ namespace Runtime.Scripts.PlayerInput
     {
         public bool IsWalking;
         public MoveDirection MoveDirection;
+    }
+
+    public enum MoveInputType
+    {
+        Direction,
+        Position
     }
 
     public enum MoveDirection
